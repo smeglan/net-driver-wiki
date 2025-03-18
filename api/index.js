717c8f170763b5bd5c9808b72713b03163c04027
smeglan <drasebs@gmail.com>
2025-03-18 15:41:39 -0500

    prepare for vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,4 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
 
-  if (req.method === "GET") {
-    res.status(200).json({ message: "API funcionando en Vercel 🚀" });
-  } else {
-    res.status(405).json({ error: "Método no permitido" });
-  }
+export function GET(request) {
+  return new Response('Hello from Vercel!');
 }
