73bb6d570c4a410b3ecfdbfe4327e9930f67d040
smeglan <drasebs@gmail.com>
2025-03-19 22:14:34 -0500

    Cors

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 const nextConfig: NextConfig = {
   async headers() {
