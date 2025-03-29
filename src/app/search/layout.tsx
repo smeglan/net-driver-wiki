1c8e57544478c80413c29f4269176f92de0c9d47
smeglan <drasebs@gmail.com>
2025-03-28 22:22:37 -0500

    fixed search domain

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import SearchBarTemplate from "@/domains/search/templates/search-bar.template";
 
 export default async function DigimonLayout({
@@ -10,7 +11,7 @@ export default async function DigimonLayout({
       <div className="flex flex-col items-center p-4">
         <SearchBarTemplate />
       </div>
-      <div>{children}</div>
+      <Suspense fallback={<div>Cargando...</div>}>{children}</Suspense>
     </div>
   );
 }
